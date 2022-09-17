d0a7300d2d345791c4e34ee66a9221625249a117
distefano_maximiliano <maximiliano.distefano@hotmail.com>
2022-09-17 14:54:18 -0300

    E commerce con formulario validado, carrito funcionando y uso de json y localstorage

--- javascript/miarchivo.js
+++ javascript/miarchivo.js
@@ -1,10 +1,8 @@
 const Clickbutton = document.querySelectorAll('.button')
 const tbody = document.querySelector('.tbody')
 let carrito = []
-if(localStorage.getItem("carrito")){
-    carrito=JSON.parse(localStorage.getItem("carrito"));
-}
-renderCarrito()
+
+
 
 Clickbutton.forEach(btn => {
     btn.addEventListener("click", addToCarritoItem)
@@ -56,6 +54,7 @@ renderCarrito()
 carritoTotal()
 }
 
+
 function renderCarrito(){
     tbody.innerHTML = ''
     carrito.map (item => {
@@ -77,20 +76,24 @@ function renderCarrito(){
         tr.innerHTML = Content;
         tbody.append(tr)
         localStorage.setItem("carrito", JSON.stringify(carrito));
-    })
 
+        tr.querySelector(".delete").addEventListener ('click', removeItemCarrito)
+        tr.querySelector(".input__elemento").addEventListener ('change', sumaCantidad)
+    })
+    carritoTotal()
 }
 
 //Funcion para Hacer funcionar el TOTAL del CARRITO
 function carritoTotal(){
     let total = 0;
-    const itemCantTotal = document.querySelector('itemCantTotal')
+    const itemCantTotal = document.querySelector('.itemCantTotal')
     console.log(itemCantTotal)
     carrito.forEach((item) =>{
         const precio = Number (item.precio.replace("$", ''))
         total = total + precio*item.cantidad
     })
     itemCantTotal.innerHTML = `Total $${total}`
+    addLocalStorage()
 
 }
 
@@ -114,3 +117,48 @@ function cargarStock(){
 }
 
 cargarStock();
+
+//Funcion para eliminar producto de carrito
+function removeItemCarrito (e){
+    //creo la constante en donde al hacer click se borrara el producto
+    const buttonDelete = e.target
+    //creo el componente padre del boton y le asigno el button delete
+    const tr = buttonDelete.closest(".itemCarrito")
+    const title = tr.querySelector('.title').textContent
+    for( i=0; i < carrito.length; i++){
+        if(carrito[i].title.trim() === title.trim() ){
+            //se usa el .splice para eliminar y se indica el elemento que quiero eliminar y la cantidad(i, 1)
+            carrito.splice(i, 1)
+        }
+    }
+    tr.remove()
+    carritoTotal()
+}
+
+//funcion para sumar cantidad en producto
+function sumaCantidad(e){
+const sumaInput = e.target
+const tr = sumaInput.closest(".itemCarrito")
+const title = tr.querySelector('.title').textContent
+carrito.forEach(item=> {
+    if (item.title.trim() === title){
+        sumaInput.value < 1 ? (sumaInput.value = 1): sumaInput.value;
+        item.cantidad = sumaInput.value;
+        carritoTotal()
+    }
+}) 
+console.log(carrito)
+}
+
+//localstorage y json
+function addLocalStorage(){
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+window.onload = function(){
+    const storage = JSON.parse(localStorage.getItem('carrito'));
+    if(storage){
+        carrito = storage;
+        renderCarrito()
+    }
+}
